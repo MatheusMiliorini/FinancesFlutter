7d77f27c9d93d5696ef2d920e3c34ad19af7275c
Matheus <matheus@aphixsoftware.com>
2024-05-29 17:07:38 +0100

    Use InkWell instead of GestureDetector

--- lib/widgets/home_screen/add_new_account_cart.dart
+++ lib/widgets/home_screen/add_new_account_cart.dart
@@ -7,18 +7,25 @@ class AddNewAccountCart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
+    return InkWell(
         onTap: onTap,
         child: const Card(
           shape: BeveledRectangleBorder(),
+          color: Colors.blueAccent,
           elevation: 2.0,
           child: Padding(
               padding: EdgeInsets.symmetric(horizontal: 10),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Text('Create New'),
-                  Icon(Icons.add),
+                  Text(
+                    'Create New',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
                 ],
               )),
         ));
